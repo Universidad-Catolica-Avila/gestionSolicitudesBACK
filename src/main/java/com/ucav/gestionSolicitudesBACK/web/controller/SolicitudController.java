bf677ea637b6ba83147bd25bea4bc430f5874929
ssanchezaUcavila <soraya.sanchez@ucavila.es>
2025-03-12 08:44:40 +0100

    Listado Solicitudes con Response

--- src/main/java/com/ucav/gestionSolicitudesBACK/web/controller/SolicitudController.java
+++ src/main/java/com/ucav/gestionSolicitudesBACK/web/controller/SolicitudController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +20,26 @@ import com.ucav.gestionSolicitudesBACK.repository.SolicitudRepository;
 public class SolicitudController {
 
     @Autowired
-    private SolicitudRepository solicitudRepository;
-    
+    private SolicitudRepository solicitudRepository;  
 
 
     private static final Logger logger = LoggerFactory.getLogger(SolicitudController.class);
 
     @Secured({"ROLE_ADMIN_CC", "ROLE_ADMIN", "ROLE_PROFESOR", "ROLE_CC", "ROLE_APROBADOR"})
     @GetMapping("/solicitudes")
-    public List<Solicitud> getSolicitudes() {
-    	List<Solicitud> resultSQL = solicitudRepository.findAll();
-    	return resultSQL;
+    public ResponseEntity<?> getSolicitudes() {
+        try {
+            List<Solicitud> resultSQL = solicitudRepository.findAll();
+
+            if (resultSQL.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No hay solicitudes disponibles");
+            }
+
+            return ResponseEntity.ok(resultSQL);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body("Error al recuperar las solicitudes: " + e.getMessage());
+        }
     }
     
     
